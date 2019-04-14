4ad569f80aa717c6ec78808bb1978eb22d3783cf
Peter Christakos <newtownfam@gmail.com>
2019-04-13 20:06:01 -0400

    Creating bursts and flows did work

--- Phase3 - Vectorization & Machine Learning/classifyFlows.py
+++ Phase3 - Vectorization & Machine Learning/classifyFlows.py
@@ -13,31 +13,58 @@ import datetime
 
 # create a burst
 class Burst:
-    def __init__(self, id, time):
-    	self.id = id
-    	self.time = time
-    flows = []
+	flows = []
+	def __init__(self, id, time):
+		self.id = id
+		self.time = time
 
 # create a flow
 class Flow:
-    def __init__(self, srcAddr, destAddr, srcPort, destPort, bytesTr, bytesRecv, pktSent, pktRecv, timeStamp, protocol):
-        self.srcAddr = srcAddr
-        self.destAddr = destAddr
-        self.srcPort = srcPort
-        self.destPort = destPort
-        self.bytesTr = bytesTr
-        self.bytesRecv = bytesRecv
-        self.pktSent = pktSent
-        self.pktRecv = pktRecv
-        self.timeStamp = timeStamp
-        self.protocol = protocol
+	def __init__(self, srcAddr, destAddr, srcPort, destPort, bytesTr, bytesRecv, pktSent, pktRecv, timeStamp, protocol):
+		self.srcAddr = srcAddr
+		self.destAddr = destAddr
+		self.srcPort = srcPort
+		self.destPort = destPort
+		self.bytesTr = bytesTr
+		self.bytesRecv = bytesRecv
+		self.pktSent = pktSent
+		self.pktRecv = pktRecv
+		self.timeStamp = timeStamp
+		self.protocol = protocol
+
+def create_flow(timesecs, pkt):
+	# if sent, add to packets sent
+	if '192.168.12' in pkt.ip.src:	
+		flow = Flow(pkt.ip.src, pkt.ip.dst, pkt[pkt.transport_layer].srcport, pkt[pkt.transport_layer].dstport, pkt.length, 0, 1, 0, timesecs, pkt.transport_layer)
+	# else, add to packets recv
+	else:
+		flow = Flow(pkt.ip.src, pkt.ip.dst, pkt[pkt.transport_layer].srcport, pkt[pkt.transport_layer].dstport, 0, pkt.length, 0, 1, timesecs, pkt.transport_layer)
+	return flow
+
+
+def update_flows(pkt, flows):
+	for flow in flows:
+		if "192.168.12" not in newFlow.srcAddr:
+			# incr recv
+			if (flow.destAddr == newFlow.srcAddr) and (flow.destPort == newFlow.srcportc) and (flow.protocol == newFlow.protocol):
+				#update connection
+				flow.bytesRecv += pkt.length
+				#flow.timeout = # TODO
+				flow.pktRecv += 1
+				match = 1
+		elif "192.168.12" in newFlow.srcAddr:
+			# incr tr
+			if (flow.destAddr == newFlow.destAddr) and (flow.destPort == newFlow.destPort) and (flow.protocol == newFlow.protocol):
+				#update connection
+				flow.bytesTr += pkt.length
+				#flow.timeout = # TODO
+				flow.pktSent += 1
+				match = 1
+	return flows
+
 
 def main():
-	
-	counter=0
-	ipcounter=0
-	tcpcounter=0
-	udpcounter=0
+
 	id = 0
 	burst = None
 	burstList = []
@@ -46,34 +73,31 @@ def main():
 
 	for file in sys.argv[1:]:
 		packets = pyshark.FileCapture(file)
-		packets_Sent = 1
-		packets_Recv = 1
 		for pkt in packets:
-			#newFlow = Flow(packet.ip.src, packet.ip.dst, packet[packet.transport_layer].srcport, 
-			#	packet[packet.transport_layer].dstport, packet.transport_layer, 
-			#	packet.sniff_timestamp, int(packet.length))
-			#time = datetime.datetime.utcfromtimestamp(ts)
-			#timesecs = time.second*1000000 + time.microsecond
+		
+			# flow match
+			match = 0
+
+			# set t ime
 			timesecs = pkt.sniff_time.second*1000000 + pkt.sniff_time.microsecond
 
+			# add create burst if new or timeout of 1s occurs
 			if id == 0 or timesecs - lastTime > 1000000:
 				burst = Burst(id, timesecs)
 				burstList.append(burst)
 
 			# add first flow
 			if len(burst.flows) == 0:
-				# if sent, incr packets_sent
-				if '192.168.12' in pkt.ip.src:	
-					flow = Flow(pkt.ip.src, pkt.ip.dst, pkt[pkt.transport_layer].srcport, pkt[pkt.transport_layer].dstport, 0, 0, packets_Sent, packets_Recv, timesecs, pkt.transport_layer)
-					packets_Sent += 1
-					# TODO INCR BYTES TR
-				# else, incr packets recv
-				else
-					flow = Flow(pkt.ip.src, pkt.ip.dst, pkt[pkt.transport_layer].srcport, pkt[pkt.transport_layer].dstport, 0, 0, packets_Sent, packets_Recv, timesecs, pkt.transport_layer)
-					packets_Recv += 1
-					# TODO INCR BYTES RECV
+				newFlow = create_flow(timesecs, pkt)
+				match = 1
+			else:	
+				burst.flows = update_Flows(pkt, burst.flows)
+				match = 1
+
+			if match is 0:
+				if '192.168.12' in newFlow.srcAddr:
+					burst.flows.append(newFlow)
 
 			id += 1
 
-			print(len(burstList))
 main()
